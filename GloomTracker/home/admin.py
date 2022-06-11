04d26f694c1f54350161558329b6fad3e2ead16c
Georgii Karat <georgy.karat@gmail.com>
2022-06-11 20:12:23 +0300

    add squad db

--- GloomTracker/home/admin.py
+++ GloomTracker/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from home.models import Squad
 # Register your models here.
+
+class SquadAdmin(admin.ModelAdmin):
+    list_display = ['id', 'squad_name', 'squad_start', 'squad_last','reputation', 'prospect', 'church']
+    search_fields = ['squad_name']
+
+admin.site.register(Squad, SquadAdmin)
--- GloomTracker/home/models.py
+++ GloomTracker/home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Squad(models.Model):
+    squad_name = models.CharField(max_length = 20)
+    squad_start = models.DateTimeField()
+    squad_last = models.DateTimeField()
+    reputation = models.IntegerField()
+    prospect = models.IntegerField()
+    church = models.IntegerField()
+
+    def __str__(self):
+        return self.access
