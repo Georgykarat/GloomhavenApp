88698462f98e267e462254af502e24b4a77a388e
Georgii Karat <georgy.karat@gmail.com>
2022-06-11 18:50:53 +0300

    create architecture for home page

--- GloomTracker/GloomTracker/settings.py
+++ GloomTracker/GloomTracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gp1a27qk23nr^_mvuw4hz3gxi6c#smm2iq$xscsjk*!__er$jk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
